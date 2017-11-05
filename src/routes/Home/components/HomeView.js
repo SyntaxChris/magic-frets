f5ac79f3db6ede94f204abda7e37415d245308b7
Chris Lehneis <cdlehneis@gmail.com>
2017-11-05 02:21:03 -0500

    adds string animation

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,6 +1,5 @@
 import _ from 'lodash'
-import React from 'react'
-import DuckImage from '../assets/Duck.jpg'
+import React, { Component } from 'react'
 import './HomeView.scss'
 
 export const FretPad = ({ fretIndex }) => {
@@ -29,25 +28,65 @@ export const FretPad = ({ fretIndex }) => {
   </div>
 }
 
-export const Fretboard = ({frets}) => <div className='fretboard'>
+class GuitarString extends Component {
+  componentDidMount() {
+    let angle = 0
+    let lastTime = null
+    const { stringIndex } = this.props
+    const svgPath = document.getElementById(`guitar-string-path-${stringIndex}`)
+    const frequency = 0.3/(stringIndex + 1)
+    const animate = (time) => {
+      //if last time is not equal to 0
+      if (lastTime != null) angle += (time - lastTime) * frequency
+      
+      lastTime = time
+
+      //y position from 225 to 275
+      let y = 1 + (Math.sin(angle) * 1)
+
+      svgPath.setAttribute('d', `M0,1 Q50,${y} 100,1`)
+
+      requestAnimationFrame(animate)
+    }
+
+    animate() 
+  }
+
+  render () {
+    const { stringIndex } = this.props 
+    return <div className='string'>
+      <svg viewBox='0 0 100 2' preserveAspectRatio='none'>
+        <path
+          id={`guitar-string-path-${stringIndex}`}
+          d='M 0,1 Q50,1 100,1'
+          strokeWidth={this.props.gauge}
+          stroke='#b7b7b7'
+          fill='none'
+        />
+      </svg>
+      <animateMotion dur="2s" repeatCount="indefinite">
+        <mpath xlinkHref={`#guitar-string-path-${stringIndex}`}/>
+      </animateMotion>
+    </div>
+  }
+}
+
+export const Fretboard = ({ frets, stringGauges }) => <div className='fretboard'>
   <div className='nut' />
   <div className='frets'>
     {_.times(frets, (i) => <FretPad key={i.toString()} fretIndex={i} />)}
   </div>
   <div className='strings'>
-    <div className='string' />
-    <div className='string' />
-    <div className='string' />
-    <div className='string' />
-    <div className='string' />
-    <div className='string' />
+    {stringGauges.map((gauge, i) => <GuitarString gauge={gauge} key={i.toString()} stringIndex={i} />)}
   </div>
 </div>
 
-export const HomeView = () => (
-  <div className='home-view'>
-    <Fretboard frets={12} />
+export const HomeView = () => {
+  const stringGauges = [0.15, 0.17, 0.19, 0.21, 0.23, 0.25]
+
+  return <div className='home-view'>
+    <Fretboard frets={12} stringGauges={stringGauges} />
   </div>
-)
+}
 
 export default HomeView
