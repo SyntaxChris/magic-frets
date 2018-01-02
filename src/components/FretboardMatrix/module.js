e86501f8789d564eed827a6782cff31e6e97d1ec
Chris Lehneis <cdlehneis@gmail.com>
2018-01-02 00:36:54 -0500

    these frequencies are kind of useless for listening to guitar string pitches

--- src/components/FretboardMatrix/module.js
+++ src/components/FretboardMatrix/module.js
@@ -128,31 +128,31 @@ const initialState = {
         { value: 'G', frequency: 440 }
       ],
       [
-        { value : 'D', frequency: 440 },
-        { value : 'D#', frequency: 440 },
-        { value : 'E', frequency: 440 },
-        { value : 'F', frequency: 440 },
-        { value : 'F#', frequency: 440 },
-        { value : 'G', frequency: 440 },
-        { value : 'G#', frequency: 440 },
+        { value : 'D', frequency: 146.83 },
+        { value : 'D#', frequency: 155.56 },
+        { value : 'E', frequency: 164.81 },
+        { value : 'F', frequency: 174.61 },
+        { value : 'F#', frequency: 185.00 },
+        { value : 'G', frequency: 196.00 },
+        { value : 'G#', frequency: 207.65 },
+        { value : 'A', frequency: 220.00 },
+        { value : 'A#', frequency: 233.08 },
+        { value : 'B', frequency: 246.94 },
+        { value : 'C', frequency: 261.63 },
+        { value : 'C#', frequency: 277.18 },
+        { value : 'D', frequency: 293.67 },
+        { value : 'D#', frequency: 311.13 },
+        { value : 'E', frequency: 329.63 },
+        { value : 'F', frequency: 349.23 },
+        { value : 'F#', frequency: 369.99 },
+        { value : 'G', frequency: 392.00 },
+        { value : 'G#', frequency: 415.30 },
         { value : 'A', frequency: 440 },
-        { value : 'A#', frequency: 440 },
-        { value : 'B', frequency: 440 },
-        { value : 'C', frequency: 440 },
-        { value : 'C#', frequency: 440 },
-        { value : 'D', frequency: 440 },
-        { value : 'D#', frequency: 440 },
-        { value : 'E', frequency: 440 },
-        { value : 'F', frequency: 440 },
-        { value : 'F#', frequency: 440 },
-        { value : 'G', frequency: 440 },
-        { value : 'G#', frequency: 440 },
-        { value : 'A', frequency: 440 },
-        { value : 'A#', frequency: 440 },
-        { value : 'B', frequency: 440 },
-        { value : 'C', frequency: 440 },
-        { value : 'C#', frequency: 440 },
-        'D'
+        { value : 'A#', frequency: 466.16 },
+        { value : 'B', frequency: 493.88 },
+        { value : 'C', frequency: 523.25 },
+        { value : 'C#', frequency: 554.37 },
+        { value : 'D', frequency: 587.33 }
       ],
       [
         { value : 'A', frequency: 110.00 },
@@ -222,10 +222,6 @@ const initialState = {
     ]
   }
 }
-
-
-
-
 
 export default function fretboardMatrixReducer (state = initialState, action) {
   const handler = ACTION_HANDLERS[action.type]
